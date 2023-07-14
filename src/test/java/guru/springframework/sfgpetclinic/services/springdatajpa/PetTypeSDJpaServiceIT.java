5b65092ab993bba0df51e7491a80fedf8c7e3738
andriipodaiko <andrij525@gmail.com>
2023-07-14 14:14:18 -0400

    added circle budges and fail one test to check it

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @ExtendWith(TimingExtension.class)
 class PetTypeSDJpaServiceIT {
 
@@ -14,6 +16,7 @@ class PetTypeSDJpaServiceIT {
 
     @Test
     void findAll() {
+        fail();
     }
 
     @Test
